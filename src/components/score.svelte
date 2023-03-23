538d3ed767e4cb0319f3910cdc64057309b2aa63
jimmycooks6748 miedema <graysenmiedema@gmail.com>
2023-03-23 19:35:05 +0000

    Test commit

---
+++ src/components/score.svelte
@@ -0,0 +1,6 @@
+<script>
+   export let score = 0;
+</script>
+
+Your score is: {score}
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Question from '../components/question.svelte';
+	import Score from '../components/score.svelte';
+</script>
+
+<Score score={14} />
+
+<Question
+	number={1}
+	answer1="First answer"
+	answer2="Second answer"
+	answer3="Third answer"
+	answer4="Fourth answer"
+>
+	This is the question
+</Question>
