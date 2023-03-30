306a6ba07ca582d3db048307d54deedf7a33aacb
jimmycooks6748 miedema <graysenmiedema@gmail.com>
2023-03-30 19:21:13 +0000

    succes finally:)

---
+++ src/components/answer.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let answer = true;
+</script>
+{#if answer}
+<p> haha you thought
+</p>
+{:else}
+<p> not quite... </p>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Question from '../components/question.svelte';
 	import Score from '../components/score.svelte';
+	import Answer from '../components/answer.svelte';
 </script>
 
 <Score score={14} />
@@ -11,6 +12,41 @@
 	answer2="Second answer"
 	answer3="Third answer"
 	answer4="Fourth answer"
+	answer="a"
 >
 	This is the question!
 </Question>
+
+<Question
+	number={1}
+	answer1="First answer"
+	answer2="Second answer"
+	answer3="Third answer"
+	answer4="Fourth answer"
+	answer="b"
+>
+	This is the question!
+</Question>
+
+<Question
+	number={1}
+	answer1="First answer"
+	answer2="Second answer"
+	answer3="Third answer"
+	answer4="Fourth answer"
+	answer="c"
+>
+	This is the question!
+</Question>
+
+<Question
+	number={1}
+	answer1="First answer"
+	answer2="Second answer"
+	answer3="Third answer"
+	answer4="Fourth answer"
+	answer="d"
+>
+	This is the question!
+</Question>
+
